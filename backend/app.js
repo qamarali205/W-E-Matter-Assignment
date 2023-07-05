7bd0b8df8ce4ab834911fbf596140b5fd8ecea86
qamarali205 <qamarali205@gmail.com>
2023-07-05 15:56:11 +0530

    api

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const Users=require('./model/userModel');
 
 dotenv.config();
 
@@ -12,14 +13,38 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(cors());
 
+//post api
+app.post('/signup', async(req,res)=>{
+    try {
+        let user = new Users(req.body);
+        //data insert in database
+        let result = await user.save();
+        // result.result.toObject();
+        // delete result.password;
+        res.send(result);
+        // console.log(req.body);
+      } catch (error) {
+        res.send({ error: "Record Not Saved" });
+      }
+});
 
-
-
-
-
-//get seats
-
-
-
+app.post('/login', async(req,res)=>{
+    //console.log(req.body)
+    try {
+        if (req.body.password && req.body.email) {
+          let user = await Users.findOne(req.body).select("-password");
+          
+          if (user) {
+            res.send(user);
+          } else {
+            res.send({ result: "No User Found" });
+          }
+        } else {
+          res.send({ result: "No User Found" });
+        }
+      } catch (error) {
+        res.send({ error: "Something error." });
+      }
+})
 
 module.exports = app;
